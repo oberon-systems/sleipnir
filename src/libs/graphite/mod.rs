5b60ae81116b007c4c7cabfbe8299fbb60829a47
zombig <me@zombig.name>
2025-11-15 20:52:09 +0100

    [feat][libs/graphite]: update metrics parsing
    	Use byte based string for parce metrics.

--- src/libs/graphite/mod.rs
+++ src/libs/graphite/mod.rs
@@ -1,63 +1,80 @@
-use std::collections::HashMap;
+use smallvec::SmallVec;
 
-#[allow(dead_code)]
-#[derive(Debug, Clone)]
-pub struct GraphiteMetric {
-    pub name: String,
-    pub tags: HashMap<String, String>,
+pub struct GraphiteMetric<'a> {
+    pub name: &'a str,
+    pub tags: SmallVec<[(&'a str, &'a str); 16]>,
     pub value: f64,
     pub timestamp: i64,
 }
 
-/*
-    Parse metric, expected metric format is
-    metric_name;tag1=val1;tag2=val2 value timestamp
-*/
-impl GraphiteMetric {
-    pub fn parse(line: &str) -> Result<Self, String> {
-        // sanitize and split
-        let parts: Vec<&str> = line.split_whitespace().collect();
+impl<'a> GraphiteMetric<'a> {
+    #[inline(always)]
+    pub fn parse(line: &'a str) -> Result<Self, String> {
+        let bytes = line.as_bytes();
+        let len = bytes.len();
 
-        // validate metric in format `metric value timestamp`
-        if parts.len() != 3 {
-            return Err("invalid metric format: expected 3 parts".to_string());
-        }
+        // try to search first two spaces
+        let (mut sp1, mut sp2) = (usize::MAX, usize::MAX);
 
-        // collect metrics part
-        let metric = parts[0];
-
-        // collect value part
-        let value: f64 = parts[1].parse().map_err(|_| "invalid value".to_string())?;
-
-        // collect timestamp
-        let timestamp: i64 = parts[2]
-            .parse()
-            .map_err(|_| "invalid timestamp".to_string())?;
-
-        // process metric
-        let mut split = metric.split(';');
-
-        // collect metric name
-        let name = split.next().ok_or("missing metric name")?.to_string();
-
-        // map tags
-        let mut tags = HashMap::new();
-        for tag in split {
-            let tag_parts: Vec<&str> = tag.split('=').collect();
-            if tag_parts.len() == 2 {
-                tags.insert(tag_parts[0].to_string(), tag_parts[1].to_string());
+        for i in 0..len {
+            if bytes[i] == b' ' {
+                if sp1 == usize::MAX {
+                    sp1 = i;
+                } else {
+                    sp2 = i;
+                    break;
+                }
             }
         }
+        if sp1 == usize::MAX || sp2 == usize::MAX {
+            return Err(format!("invalid graphite line: {:?}", line));
+        }
 
-        // return processed data
-        Ok(GraphiteMetric {
-            name,
-            tags,
-            value,
-            timestamp,
-        })
+        // collect metric (path), value and timestamp
+        let metric = &line[..sp1];
+        let value_str = &line[sp1 + 1 .. sp2];
+        let ts_str = &line[sp2 + 1 ..];
+
+        // convert value and timestamp
+        let value: f64 = value_str.parse().map_err(|_| "bad value")?;
+        let timestamp: i64 = ts_str.parse().map_err(|_| "bad ts")?;
+
+        // parse metric (path)
+        let mb = metric.as_bytes();
+        let mlen = mb.len();
+        let mut i = 0;
+
+        // collect name
+        while i < mlen && mb[i] != b';' {
+            i += 1;
+        }
+        let name = &metric[..i];
+
+        // ... collect tags
+        let mut tags = SmallVec::<[(&str, &str); 16]>::new();
+
+        while i < mlen {
+            i += 1; // skip ';'
+            if i >= mlen { break; }
+
+            let key_start = i;
+            while i < mlen && mb[i] != b'=' { i += 1; }
+            if i >= mlen { break; }
+            let key_end = i;
+
+            i += 1; // skip '='
+            if i >= mlen { break; }
+
+            let val_start = i;
+            while i < mlen && mb[i] != b';' { i += 1; }
+            let val_end = i;
+
+            tags.push((
+                &metric[key_start..key_end],
+                &metric[val_start..val_end],
+            ));
+        }
+
+        Ok(Self { name, tags, value, timestamp })
     }
 }
-
-#[cfg(test)]
-mod tests;
